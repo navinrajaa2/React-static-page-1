646513dd300e69ab0584e94f94daf05956c4b8d6
navinrajaa2 <navinrajaa02@gmail.com>
2026-02-19 18:36:37 +0530

    static page 1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./compent/Navbar";
+import { Header } from "./compent/Header";
+import { Fragment } from "react";
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
+}
---
+++ src/compent/Navbar.jsx
@@ -0,0 +1,12 @@
+export function Navbar() {
+    return (
+        <main>
+        <header>
+            <nav>
+                <img src="./images/images.png" alt="react logo"/>
+                <span className="">ReactFacts</span>
+            </nav>
+        </header>
+        </main>
+    )
+}
